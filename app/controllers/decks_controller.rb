25090a5935184c6b4097bb152772d7b782826234
Matt Gainer <matgainer@gmail.com>
2016-01-18 22:28:46 -0800

    syntax error in decks

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,13 +1,12 @@
 class DecksController < ApplicationController
   before_action :authenticate_their_deck!, only: [:update, :create]
   before_action :check_deck_removed!, only: [:show, :edit]
-  before_action :must_sign_up!, 
+  before_action :must_sign_up!
   def index
     @decks = Deck.decks_by_format
   end
 
   def show
-
     @deck = Deck.find(params[:id])
     @comments = []
   end
